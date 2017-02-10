9bc5f5d0633f55fdca22df95791f19b3f3057f1f
kori-irrlicht <kori.irrlicht.dev@gmail.com>
2017-02-10 23:24:29 +0100

    Add rule update command

--- commands.go
+++ commands.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
@@ -14,13 +15,17 @@ func messageCreateHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 	user, _ := s.User("@me")
 	if m.Author.ID != user.ID {
+		ch, _ := s.Channel(m.ChannelID)
+		if ch.Name == "regeln" && !strings.HasPrefix(m.Content, commandPrefix) {
+			s.ChannelMessageDelete(ch.ID, m.ID)
+			return
+		}
 		if !strings.HasPrefix(m.Content, commandPrefix) {
 			return
 		}
 
 		strAr := strings.Split(m.Content, " ")
 		str := strings.Replace(strAr[0], commandPrefix, "", 1)
-		ch, _ := s.Channel(m.ChannelID)
 		if ch.Name == "regeln" {
 
 			switch str {
@@ -29,10 +34,27 @@ func messageCreateHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 				for _, rl := range rollen {
 					fmt.Println(rl.ID)
 				}
-				fmt.Println(s.GuildRoles(ch.GuildID))
 				err := s.GuildMemberRoleAdd(ch.GuildID, m.Author.ID, viper.GetString("mitgliedRollenId"))
-				fmt.Println(err)
+				if err != nil {
+					fmt.Println(err)
+				}
+				s.ChannelMessageDelete(ch.ID, m.ID)
+
+			case "update":
+				// Remove all message from the channel
+				// and update the rules
+				msgs, err := s.ChannelMessages(ch.ID, 100, "999999999999999999", "000000000000000000")
+				if err != nil {
+					fmt.Println(err)
+				}
+				for _, msg := range msgs {
+					s.ChannelMessageDelete(ch.ID, msg.ID)
+				}
+				checkRuleEntry(s)
+			default:
+				s.ChannelMessageDelete(ch.ID, m.ID)
 			}
+
 		}
 
 		switch str {
@@ -41,4 +63,36 @@ func messageCreateHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 		}
 
 	}
+}
+
+func checkRuleEntry(s *discordgo.Session) (err error) {
+	rules := viper.GetString("channel.rules")
+
+	msgs, err := s.ChannelMessagesPinned(rules)
+	if err != nil {
+		return err
+	}
+
+	file, err := ioutil.ReadFile(viper.GetString("regeln"))
+	if err != nil {
+		return err
+	}
+
+	if len(msgs) == 0 {
+		for i := 0; i < len(file); i += 2000 {
+
+			max := i + 2000
+			if max > len(file) {
+				max = len(file)
+			}
+
+			_, err := s.ChannelMessageSend(rules, string(file[i:max]))
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+
 }
